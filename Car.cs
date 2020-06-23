2f965f523ac821c2ed5c292bca2ebe52449bdb27
Joel Timothy <67300294+JoelsTimothy@users.noreply.github.com>
2020-06-22 19:03:12 -0700

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        static void Main(string[] args)
+        {
+            //int age;
+            //string name;
+            MSSACar myWorkCar = new MSSACar(); //creating an instance of MSSACAr
+
+
+            Console.WriteLine("what is the color of the car?: ");
+            myWorkCar.Color = Console.ReadLine();
+
+            myWorkCar.Color = CarColors.Black;
+            myWorkCar.Year = 2025;
+            //myWorkCar._ye
+            myWorkCar.Make = CarMake.Mazda;
+            //myWorkCar.TotalMiles = 0;  //not allowed
+            myWorkCar.MPG = 30;
+            myWorkCar.FuelLevel = 25;
+            myWorkCar.IsFirstOwner = true;
+            myWorkCar.Model = "SUV";
+
+
+
+
+            myWorkCar.Drive(20);
+            myWorkCar.PrintMe();
+            myWorkCar.Drive(100);
+            myWorkCar.PrintMe();
+
+            MSSACar myWishCar = CallMeMethod();
+            myWishCar.Drive(1000);
+            myWishCar.PrintMe();
+
+            Console.WriteLine(myWorkCar);
+            Console.WriteLine(myWishCar);
+
+        }
+
+
+        public static MSSACar CallMeMethod()
+        {
+            MSSACar myDreamCar = new MSSACar(CarColors.Red, 1972, CarMake.Chevy, 12, 15.3, false, "Camaro"); //calling the non-default constructor
+
+
+            myDreamCar.PrintMe();
+            //Console.WriteLine(CarColors.Green);
+            return myDreamCar;
+        }
+
+    }
+
+        
+    
+        
+        enum CarColors { Black, Blue, Yellow, Red, Green, White };
+    enum CarMake { Toyota, Ford, Chevy, Acura, BMW, Mercedes, Mazda, Buick, Other };
+
+    struct MSSACar  //a custom defined type
+    {
+        //store characteristics: properties, fields   Color, Year, Make, Model, TotalMiles, MPG, FuelLevel, FirstOwner
+        public CarColors Color { get; set; }
+
+        private int _year;
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                if (value < 1920)
+                    _year = 1920;
+                else if (value > 2021)
+                    _year = 2021;
+                else
+                    _year = value;
+            }
+        }
+
+        public CarMake Make { get; set; }
+        public int TotalMiles { get; private set; }
+        public int MPG { get; set; }
+        public double FuelLevel { get; set; } //0 means empty, ... up to 
+        public bool IsFirstOwner { get; set; }
+        public string Model { get; set; }
+        //public int VIN{ get; set; }
+        //private static int counter;
+
+        //method
+        public void Drive(int numberOfMiles)
+        {
+            if (FuelLevel > 0)
+            {
+                TotalMiles += numberOfMiles;
+                FuelLevel -= (numberOfMiles / MPG);
+            }
+
+        }
+
+        public void PrintMe()
+        {
+            Console.WriteLine("=========================== displaying a car =======================");
+            Console.WriteLine($"Color = {Color}, year = {Year}, make = {Make}");
+            Console.WriteLine($"Number of miles: {TotalMiles}, MPG = {MPG}, Fuel level: {FuelLevel}");
+            Console.WriteLine($"is first owner? {IsFirstOwner}, model = {Model}");
+            Console.WriteLine("====================================================================");
+        }
+
+        public override string ToString()
+        {
+            return String.Format("=========================== displaying a car =======================\n"
+                + "Color = {0}, year = {1}, make = {2}\n"
+                + "Number of miles: {3}, MPG = {4}, Fuel level: {5}\n"
+                + "is first owner? {6}, model = {7}\n"
+                + "====================================================================", Color, Year, Make, TotalMiles, MPG, FuelLevel, IsFirstOwner, Model);
+        }
+
+        //constructor
+        public MSSACar(CarColors color, int year, CarMake make, int mpg, double fuelLevel, bool IsFirstOwner, string Model)
+        {
+            Color = color;
+            _year = year;
+            Make = make;
+            this.TotalMiles = 0;
+            this.MPG = mpg;
+            this.FuelLevel = fuelLevel;
+            this.IsFirstOwner = IsFirstOwner;
+            this.Model = Model;
+            //counter++;
+            //VIN = counter;
+        }
+
+    }
+
+
+
+}
+
---
+++ GymLong.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
+using System.Threading;
+using System.Net.Cache;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[10] { 1234, 2345, 3456, 4567, 5678, 6789, 7890, 8901, 9012, 0123 };
+            Console.WriteLine(numbers[0]);
+
+            Console.WriteLine("Welcome to Golds Gym!");
+            Console.Write("Are you a Gold Customer? True or False: ");
+            bool isGoldCustomer = Convert.ToBoolean(Console.ReadLine());
+
+            float price;
+            if (isGoldCustomer)
+            {
+                MembershipNumber1();
+            }
+            else
+            {
+                price = 29.95f;
+                Console.WriteLine("Welcome to Golds Gym!  Your entrance fee will be {0}", price);
+            }
+        }
+
+        //### activate: private static void MembershipNumber()
+
+        //private static void MembershipNumber()
+        {
+            int[] numbers = new int[9] { 1234, 2345, 3456, 4567, 5678, 6789, 7890, 8901, 9012 };
+            Console.Write("Please type in your 4 digit membership number: ");
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            float price;
+            if (result.Equals(numbers[0]) || result.Equals(numbers[1]) || result.Equals(numbers[2]))
+            {
+                price = 19.95f;
+                Console.WriteLine("Welcome Gold Member!  Your entrance fee today is only {0}", price);
+            }
+            else if (result.Equals(numbers[3]) || result.Equals(numbers[4]) || result.Equals(numbers[5]))
+            {
+                price = 19.95f;
+                Console.WriteLine("Welcome Gold Member!  Your entrance fee today is only {0}", price);
+            }
+            else if (result.Equals(numbers[6]) || result.Equals(numbers[7]) || result.Equals(numbers[8]))
+            {
+                price = 19.95f;
+                Console.WriteLine("Welcome Gold Member!  Your entrance fee today is only {0}", price);
+            }
+
+            else
+            {
+                LoginAttempt1();  
+            }
+
+            Console.WriteLine("Thank you for coming in!");
+
+        }
+
+        //### activate: private static void LoginAttempt1()
+
+        //private static void LoginAttempt1()   
+        {
+            int loginAttempts = 0;
+            float price;
+            for (int i = 1; i < 3; i++)
+            {
+                int[] numbers = new int[9] { 1234, 2345, 3456, 4567, 5678, 6789, 7890, 8901, 9012 };
+                Console.WriteLine("Incorrect membership number");
+                Console.Write("Please type in your 4 digit membership number again: ");
+                int result = Convert.ToInt32(Console.ReadLine());
+
+                if (result.Equals(numbers[0]) || result.Equals(numbers[1]) || result.Equals(numbers[2]))
+                {
+                    price = 19.95f;
+                    Console.WriteLine("Welcome Gold Member!  Your entrance fee today is only {0}", price);
+                    break;
+                }
+                else if (result.Equals(numbers[3]) || result.Equals(numbers[4]) || result.Equals(numbers[5]))
+                {
+                    price = 19.95f;
+                    Console.WriteLine("Welcome Gold Member!  Your entrance fee today is only {0}", price);
+                    break;
+                }
+                else if (result.Equals(numbers[6]) || result.Equals(numbers[7]) || result.Equals(numbers[8]))
+                {
+                    price = 19.95f;
+                    Console.WriteLine("Welcome Gold Member!  Your entrance fee today is only {0}", price);
+                    break;
+                }
+                else
+                    loginAttempts++;
+
+            }
+
+            if (loginAttempts > 1)
+            {
+                price = 29.95f;
+                Console.WriteLine("Login Failure.  We cannot confirm your membership.  Please confirm with staff, or pay todays entrance fee of {0}", price);
+            }
+
+        }
+
+    }
+}
